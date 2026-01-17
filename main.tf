95a9acfc7e65dab9af405728d04a50bdb2d0395b
asabhaysingh328 <asabhaysingh328@gmail.com>
2026-01-17 14:41:46 +0530

    Letest IP change 35

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "azurerm_virtual_machine" "vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.27-LTS"
+    sku       = "18.35-LTS"
     version   = "latest"
   }
 
